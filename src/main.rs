40a4ec083382ba75f55450107818c6ff652f8bc9
Ueki Masaru <masaru.ueki@itage.co.jp>
2018-10-26 18:20:12 +0900

    use async/await

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,20 @@
-use tokio::prelude::*;
+#![feature(pin, await_macro, async_await, futures_api)]
+use tokio::await;
 use tokio::net::UdpSocket;
 use std::net::SocketAddr;
 
-use futures::try_ready;
-
-struct EchoServer {
-    socket: UdpSocket,
-    buf: Vec<u8>,
-}
-
-impl EchoServer {
-    fn new(socket: UdpSocket) -> EchoServer {
-        EchoServer{socket: socket, buf: vec![0; 1024]}
-    }
-}
-
-impl Future for EchoServer {
-    type Item = ();
-    type Error = std::io::Error;
-
-    fn poll(&mut self) -> Poll<(), std::io::Error> {
-        let _ = try_ready!(self.socket.poll_recv_from(&mut self.buf));
-        println!("connect");
-        Ok(Async::Ready(()))
-    }
-}
-
 fn main() {
     let addr = "127.0.0.1:9000".to_string().parse::<SocketAddr>().unwrap();
-    let socket = UdpSocket::bind(&addr).unwrap();
+    let mut socket = UdpSocket::bind(&addr).unwrap();
     println!("Listening on: {}", socket.local_addr().unwrap());
 
-    let server = EchoServer::new(socket);
-
-    tokio::run(server.map_err(|_| ()));
+    tokio::run_async(async move {
+        loop {
+            let buf = vec![0; 1024];
+            let (sock, buf, size, peer) = await!(socket.recv_dgram(buf)).unwrap();
+            let (sock, _) = await!(sock.send_dgram(buf, &peer)).unwrap();
+            socket = sock;
+            println!("Echoed {} to {}", size, peer);
+        }
+    });
 }
