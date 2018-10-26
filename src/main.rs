83d106a9ef0f39de067d6bf1ab7d8e238b2b67f5
Ueki Masaru <masaru.ueki@itage.co.jp>
2018-10-26 16:03:28 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use tokio::prelude::*;
+use tokio::net::UdpSocket;
+use std::net::SocketAddr;
+
+struct EchoServer {
+    socket: UdpSocket,
+    buf: Vec<u8>,
+}
+
+impl EchoServer {
+    fn new(socket: UdpSocket) -> EchoServer {
+        EchoServer{socket: socket, buf: vec![0; 1024]}
+    }
+}
+
+impl Future for EchoServer {
+    type Item = ();
+    type Error = ();
+
+    fn poll(&mut self) -> Poll<(), ()> {
+        match self.socket.poll_recv_from(&mut self.buf) {
+            Ok(Async::Ready((_size, _peer))) => {
+                println!("recv");
+                Ok(Async::Ready(()))
+            },
+            Ok(Async::NotReady) => Ok(Async::NotReady),
+            Err(_) => Err(()),
+        }
+    }
+}
+
+fn main() {
+    let addr = "127.0.0.1:9000".to_string().parse::<SocketAddr>().unwrap();
+    let socket = UdpSocket::bind(&addr).unwrap();
+    println!("Listening on: {}", socket.local_addr().unwrap());
+
+    let server = EchoServer::new(socket);
+
+    tokio::run(server);
+}
